ec9d5da4e574d479fb7e60a4d388da9fa76703a9
thotranuet97 <thotranuet97@gmail.com>
2018-04-22 08:59:20 +0000

    last commit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :idea
     belongs_to :user
-    validates_presence_of :user_name, :body
+    validates_presence_of :body
     mount_uploader :picture, PictureUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   resources :ideas
   root 'ideas#index'
   match '/user.:id',     to: 'users#show',       via: 'get'
+  resources :ideas, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
